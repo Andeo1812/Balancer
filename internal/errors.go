4c6017e2ce7f3d17569deb8b13bf8559ec4271e0
Andeo1812 <alexanforall@mail.ru>
2023-05-07 01:15:59 +0300

    [main] app level end

--- internal/errors.go
+++ internal/errors.go
@@ -3,7 +3,8 @@ package internal
 import "errors"
 
 var (
-	ErrBadRequest        = errors.New("bad request")
+	ErrBadRequest = errors.New("bad request")
+
 	ErrGetEasyJSON       = errors.New("err get easyjson")
 	ErrJSONUnexpectedEnd = errors.New("unexpected end of JSON input")
 )
